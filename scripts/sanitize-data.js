01aeaede37a2a8ef75340d9f20fddd97001b8371
Abbondanzo <peter@abbondanzo.com>
2022-03-06 02:42:17 -0500

    Sanitize photo names

---
+++ scripts/sanitize-data.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const DATA_DIR = path.join(__dirname, "..", "data");
+
+const sanitizePhotoNames = () => {
+  const photoDir = path.join(DATA_DIR, "photos");
+  const files = fs.readdirSync(photoDir);
+  files.forEach((fileName, index) => {
+    const extensionMatch = fileName.match(/\.([A-z0-9]+)$/);
+    const extension = extensionMatch[1];
+    const newFileName = `${String(index + 1).padStart(
+      2,
+      "0"
+    )}.${extension.toLowerCase()}`;
+    fs.renameSync(
+      path.join(photoDir, fileName),
+      path.join(photoDir, newFileName)
+    );
+  });
+};
+
+sanitizePhotoNames();
