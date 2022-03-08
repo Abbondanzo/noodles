6d22d6b71ffa5cd3b829c859e46f97f92e009abe
Peter Abbondanzo <peter@abbondanzo.com>
2022-03-07 22:49:33 -0500

    Full view entry (#8)
    * Animation, not transition
    
    * Fill out brand row with video count, subscribers, and subscribe button
    
    * Better data generation
    
    * Display categories and related videos
    
    * Add horizontal support to card mixin
    
    * Separate card styles
    
    * Add sidebar videos to sidebar
    
    * Fix styling around titles

--- src/context.js
+++ src/context.js
@@ -4,12 +4,26 @@ const path = require("path");
 const ROOT_DIR = path.resolve(__dirname, "..");
 const DATA_FILE_PATH = path.join(ROOT_DIR, "literally-nosql.json");
 
+/**
+ * @param {number} num
+ * @returns {string}
+ */
+const truncateNumber = (num) => {
+  if (num > 1e6) {
+    return Math.floor(num / 1e6) + "M";
+  } else if (num > 1e3) {
+    return Math.floor(num / 1e3) + "K";
+  } else {
+    return num.toString();
+  }
+};
+
 const getContext = () => {
   if (!fs.existsSync(DATA_FILE_PATH)) {
     throw new Error("Data file has not been generated");
   }
   const json = fs.readFileSync(DATA_FILE_PATH);
-  return JSON.parse(json);
+  return { ...JSON.parse(json), truncateNumber };
 };
 
 module.exports = {
